0a4a4fb4005dfa95e4eab623f5ee3ee7a02c2218
Sébastien Dumont <mailme@sebastiendumont.com>
2018-01-26 20:53:47 +0100

    Updated JS

--- assets/js/alnp-facebook-pixel-tracking.js
+++ assets/js/alnp-facebook-pixel-tracking.js
@@ -3,7 +3,7 @@
   $('body').on( 'alnp-post-changed', function( e, post_title, post_url, post_id ) {
     fbq('track', 'PageView', {
       "source": "auto-load-next-post",
-      "version": "1.4.9",
+      "version": alnp_fb_pixel.alnpVersion,
       "pluginVersion": alnp_fb_pixel.pluginVersion
     });
   });
